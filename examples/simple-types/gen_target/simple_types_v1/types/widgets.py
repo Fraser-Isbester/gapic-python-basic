3cbec82f08fd1125c44205b7861c0658175ce2d5
Fraser <fraserisbester@gmail.com>
2023-09-02 17:24:11 -0700

    add proto + basic target

---
+++ examples/simple-types/gen_target/simple_types_v1/types/widgets.py
@@ -0,0 +1,44 @@
+import dataclasses
+import datetime
+from typing import Tuple
+from enum import Enum, auto
+
+@dataclasses.dataclass
+class Widget:
+    """The Widget type defines a Widget object
+    
+    # usage
+    ```python
+    from simple_types_v1.types import Widget
+
+    def main():
+        widget = Widget(
+            "Toaster",
+            Widget.Size.SMALL,
+            datetime.date(2020, 1, 1),
+            ("red", "blue")
+        )
+        print(widget.name)
+    ```
+    """
+
+    # The name of the widget
+    name: "str"
+    # The size of the widget
+    size: "Size"
+    # The date the widget was created
+    created: "datetime.date"
+    # The colors available for the widget
+    color: "Tuple[str]"
+
+    class Size(Enum):
+        """The WidgetSize enum defines the size of the widget"""
+
+        # The widget size is unknown
+        UNKNOWN = auto()
+        # The widget is small
+        SMALL = auto()
+        # The widget is medium
+        MEDIUM = auto()
+        # The widget is large
+        LARGE = auto()
