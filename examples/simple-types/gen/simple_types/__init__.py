b7fbc43942a90f1a9dfe8a2d8c9b5fcb4e89ce3f
Fraser <fraserisbester@gmail.com>
2023-09-09 19:10:44 -0700

    add buf configs

--- examples/simple-types/gen/simple_types/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from simple_types_v1.types.widgets import Widget
-from simple_types_v1.types.widgets import WidgetFactory
-
-
-__all__ = ('Widget',
-    'WidgetFactory',
-)
